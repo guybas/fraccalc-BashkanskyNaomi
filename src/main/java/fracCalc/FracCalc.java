fb49a047312e2f4bac0f82c0ffc6f6a695411ce4
s-bashkanskyn <s-bashkanskyn@53S500034608.it.bsd405.org>
2018-11-26 10:26:11 -0800

    Checkpoint 3

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -6,14 +6,13 @@
 package fracCalc;
 import java.util.*;
 //Naomi Bashkansky P.2
-//Checkpoint 2 prints the second number as 3 integers: the whole number, the numerator,
-//and the denominator
+//Checkpoint 3 accepts two numbers and an operator and prints out the answer of the
+//expression, though not necessarily as a reduced, mixed fraction
 	/*Pre: User types in a number made of either an int; an int, a /, and an int;
 		or an int, an _, an int, a slash, and an int. Then, a space, an operator, another
 		space, another number, and enters. User keeps typing such a line until "quit"
-		is entered. (e.g. 1_1/2 + 1/4)
-	  Post: Prints the second number as a whole, a denominator, and a numerator
-	 	(whole:0 numerator:1 denominator:4)
+		is entered. (e.g. -1_1/2 + 1/4)
+	  Post: Prints the result of the expression
 	*/
 public class FracCalc {
 
@@ -36,12 +35,13 @@ public class FracCalc {
     
     public static String produceAnswer(String input) {
         // Separates the first number, the operator, and the second number,
-    	//and parses both operands while only returning the second one
+    	//and returns the result of the expression (though not necessarily simplified)
     	int firstIndexSpace = input.indexOf(" ");
     	String first = input.substring(0, firstIndexSpace);
     	input = input.substring(firstIndexSpace + 1);
     	char operator = input.charAt(0);
     	String second = input.substring(2);
+    	
     	boolean underscore = false;
     	boolean slash = false;
     	for (int index = 0; index < first.length(); index++) {
@@ -59,16 +59,19 @@ public class FracCalc {
     	if (underscore && slash) {
     		whole1 = Integer.parseInt(first.substring(0, indexU));
     		numerator1 = Integer.parseInt(first.substring(indexU+1, indexS));
+    		if (whole1 < 0) {
+    			numerator1 *= -1;
+    		}
     		denominator1 = Integer.parseInt(first.substring(indexS+1));
+    		numerator1 += whole1 * denominator1;
     	} else if (!underscore && slash) {
-    		whole1 = 0;
     		numerator1 = Integer.parseInt(first.substring(0, indexS));
     		denominator1 = Integer.parseInt(first.substring(indexS+1));
     	} else {
-    		whole1 = Integer.parseInt(first);
-    		numerator1 = 0;
     		denominator1 = 1;
+    		numerator1 = Integer.parseInt(first);
     	}
+    	
     	underscore = false;
     	slash = false;
     	for (int index = 0; index < second.length(); index++) {
@@ -86,16 +89,32 @@ public class FracCalc {
     	if (underscore && slash) {
     		whole2 = Integer.parseInt(second.substring(0, indexU));
     		numerator2 = Integer.parseInt(second.substring(indexU+1, indexS));
+    		if (whole2 < 0) {
+    			numerator2 *= -1;
+    		}
     		denominator2 = Integer.parseInt(second.substring(indexS+1));
+    		numerator2 += whole2 * denominator2;
     	} else if (!underscore && slash) {
-    		whole2 = 0;
     		numerator2 = Integer.parseInt(second.substring(0, indexS));
     		denominator2 = Integer.parseInt(second.substring(indexS+1));
     	} else {
-    		whole2 = Integer.parseInt(second);
-    		numerator2 = 0;
+    		numerator2 = Integer.parseInt(second);
     		denominator2 = 1;
-    	}    	
-        return "whole:" + whole2 + " numerator:" + numerator2 + " denominator:" + denominator2;
+    	}
+    	
+    	int answerNumerator;
+    	int answerDenominator = denominator1 * denominator2;
+    	if (operator == '+') {
+    		answerNumerator = numerator1 * denominator2 + numerator2 * denominator1;
+    	} else if (operator == '-') {
+    		answerNumerator = numerator1 * denominator2 - numerator2 * denominator1;
+    	} else if (operator == '*') {
+    		answerNumerator = numerator1 * numerator2;
+    	} else {
+    		answerNumerator = numerator1 * denominator2;
+    		answerDenominator = denominator1 * numerator2;
+    	}
+        
+    	return "" + answerNumerator + "/" + answerDenominator;
     }
 }
